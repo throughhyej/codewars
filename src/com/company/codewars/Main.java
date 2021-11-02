ca9c1984dcaaee621e7783e7e21bb5b35eec8393
throughhyej <hyejeyh@gmail.com>
2021-11-03 00:00:03 +0900

    1. StringBuilder.reverse() 2. List<String>.stream().collect(Collectors.joining(" "))

--- src/com/company/codewars/Main.java
+++ src/com/company/codewars/Main.java
@@ -1,10 +1,16 @@
 package com.company.codewars;
 
 import codewars.BitCounting;
+import codewars.SpinWords;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(new BitCounting().countBits(5));
+        /* 2. SpinWords */
+        System.out.println(new SpinWords().spinWords("Hey fellow warriors"));
+
+        /* 1. BitCounting */
+        // System.out.println(new BitCounting().countBits(5));
+
     }
 }
