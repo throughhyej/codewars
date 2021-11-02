b0a7e1ab8e526b2329b2a1a57bd906b9fa431f49
throughhyej <hyejeyh@gmail.com>
2021-11-03 00:17:55 +0900

    1. StringBuilder.reverse() 2. List<String>.stream().collect(Collectors.joining(" "))

---
+++ src/codewars/SpinWords.java
@@ -0,0 +1,31 @@
+package codewars;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SpinWords {
+    /**
+     * 2021.11.02
+     * Stop gninnipS My sdroW!
+     * https://www.codewars.com/kata/5264d2b162488dc400000001/train/java
+     */
+    public String spinWords(String sentence) {
+        String[] strArry = sentence.split(" ");
+        String[] result = new String[strArry.length];
+        String str = "";
+        for(int i=0; i<strArry.length; i++) {
+            String word = strArry[i];
+            if (word.length() >= 5) {
+                StringBuilder piece = new StringBuilder();
+                piece.append(word);
+                str = piece.reverse().toString();
+            }else {
+                str = word;
+            }
+            result[i] = str;
+        }
+        List<String> strings = Arrays.asList(result);
+        return strings.stream().collect(Collectors.joining(" "));
+    }
+}
